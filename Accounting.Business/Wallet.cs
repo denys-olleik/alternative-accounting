e16727b31f6facdd2988597d00234955e15519f4
Denys Olleik <dolleik@gmail.com>
2025-06-16 01:41:08 -0400

    ref

---
+++ Accounting.Business/Wallet.cs
@@ -0,0 +1,15 @@
+﻿using Accounting.Common;
+
+namespace Accounting.Business
+{
+  public class Wallet : IIdentifiable<int>
+  {
+    public int WalletID { get; set; }
+    public string? PublicId { get; set; }
+    public string? Password { get; set; }
+    public decimal Balance { get; set; }
+    public DateTime Created { get; set; }
+
+    public int Identifiable => WalletID;
+  }
+}
