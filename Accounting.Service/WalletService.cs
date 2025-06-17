e83186f882247dcbaa56fd2567a617af6333f759
Denys Olleik <dolleik@gmail.com>
2025-06-16 21:54:15 -0400

    ref

---
+++ Accounting.Service/WalletService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Service
+{
+  public class WalletService : BaseService
+  {
+    public WalletService() : base()
+    {
+
+    }
+    public WalletService(string databaseName, string databasePassword) : base(databaseName, databasePassword)
+    {
+
+    }
+  }
+}
---
+++ Accounting/Controllers/WalletController.cs
@@ -0,0 +1,31 @@
+﻿using Accounting.Business;
+using Accounting.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Controllers
+{
+  [Route("wallet")]
+  public class WalletController : BaseController
+  {
+    private readonly WalletService _walletService;
+
+    public WalletController(RequestContext requestContext, WalletService walletService)
+    {
+      _walletService = new WalletService(requestContext.DatabaseName, requestContext.DatabasePassword);
+    }
+
+    [Route("transfer")]
+    [HttpGet]
+    public async Task<IActionResult> Transfer()
+    {
+      return View();
+    }
+
+    //[Route("transfer")]
+    //[HttpPost]
+    //public async Task<IActionResult> Transfer(TransferViewModel model)
+    //{
+    //  _walletService.TransferAsync(model.PublicId, model.DestinationWalletPublicId)
+    //}
+  }
+}
