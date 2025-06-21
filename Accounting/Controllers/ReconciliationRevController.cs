ecfa1822b1c669ed242486a5116ba49563a681f9
Denys Olleik <dolleik@gmail.com>
2025-06-21 01:59:22 -0400

    ref

---
+++ Accounting/Controllers/ReconciliationRevController.cs
@@ -0,0 +1,16 @@
+﻿using Accounting.CustomAttributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Controllers
+{
+  [Route("reconciliations")]
+  [AuthorizeWithOrganizationId]
+  public class ReconciliationRevController 
+    : BaseController
+  {
+    public async Task<IActionResult> Reconciliations()
+    {
+      return View();
+    }
+  }
+}
