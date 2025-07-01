fe209578ae8fd2360225040692a430d5a27c5113
Denys Olleik <dolleik@gmail.com>
2025-07-01 00:02:29 -0400

    ref

--- Accounting/Models/UserViewModels/UpdateUserViewModel.cs
+++ Accounting/Models/UserViewModels/UpdateUserViewModel.cs
@@ -48,6 +48,24 @@ namespace Accounting.Models.UserViewModels
       {
         RuleFor(x => x.UserID).GreaterThan(0);
         RuleFor(x => x.Email).EmailAddress().NotEmpty();
+
+        RuleFor(x => x)
+          .Custom((model, context) =>
+          {
+            if (model.SelectedRoles != null && model.SelectedRoles.Contains(Common.UserRoleClaimConstants.TenantManager))
+            {
+              var orgIds = (model.SelectedOrganizationIdsCsv ?? "")
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(id => id.Trim())
+                .Where(id => !string.IsNullOrEmpty(id))
+                .ToList();
+
+              if (orgIds.Count == 0)
+              {
+                context.AddFailure("SelectedOrganizationIdsCsv", "At least one organization must be selected when assigning the TenantManager role.");
+              }
+            }
+          });
       }
     }
   }
