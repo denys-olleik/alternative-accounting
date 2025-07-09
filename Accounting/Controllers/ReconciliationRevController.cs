cdd7174688e84cafa4916bdbcc6b8b2667507d6e
Denys Olleik <dolleik@gmail.com>
2025-07-08 22:17:04 -0400

    ref

--- Accounting/Controllers/ReconciliationRevController.cs
+++ Accounting/Controllers/ReconciliationRevController.cs
@@ -1,5 +1,9 @@
-﻿using Accounting.CustomAttributes;
+﻿using Accounting.Business;
+using Accounting.CustomAttributes;
+using Accounting.Models.ReconciliationViewModels;
+using Accounting.Service;
 using Microsoft.AspNetCore.Mvc;
+using static Accounting.Models.ReconciliationViewModels.ReconciliationsViewModel;
 
 namespace Accounting.Controllers
 {
@@ -8,9 +12,40 @@ namespace Accounting.Controllers
   public class ReconciliationRevController 
     : BaseController
   {
+    private readonly ReconciliationService _reconciliationService;
+
+    public ReconciliationRevController(
+      RequestContext requestContext)
+    {
+      _reconciliationService = new (requestContext.DatabaseName, requestContext.DatabasePassword);
+    }
+
+    [Route("reconciliationsrev")]
+    [HttpGet]
     public async Task<IActionResult> Reconciliations()
     {
-      return View();
+      List<Reconciliation> reconciliations
+          = await _reconciliationService.GetAllDescendingAsync(20, GetOrganizationId()!.Value);
+
+      var model = new ReconciliationsViewModel();
+      model.Reconciliations = new List<ReconciliationViewModel>();
+
+      foreach (var reconciliation in reconciliations)
+      {
+        var reconciliationViewModel = new ReconciliationViewModel
+        {
+          ID = reconciliation.ReconciliationID,
+          Status = reconciliation.Status,
+          OriginalFileName = Path.GetFileName(reconciliation.ReconciliationAttachment.OriginalFileName),
+          Created = reconciliation.Created,
+          CreatedById = reconciliation.CreatedById,
+          OrganizationId = reconciliation.OrganizationId
+        };
+
+        model.Reconciliations.Add(reconciliationViewModel);
+      }
+
+      return View(model);
     }
   }
 }
