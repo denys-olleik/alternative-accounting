1bfde47ba775b46904e5fe3815e2827b80c0bfc8
Denys Olleik <dolleik@gmail.com>
2025-07-25 13:42:07 -0400

    ref

--- Accounting/Models/ReconciliationViewModels/ReconciliationTransactionsPaginatedViewModel.cs
+++ Accounting/Models/ReconciliationViewModels/ReconciliationTransactionsPaginatedViewModel.cs
@@ -8,6 +8,16 @@
     public string? OriginalFileName { get; set; }
     public int CreatedById { get; set; }
     public int OrganizationId { get; set; }
+
+    public List<AccountViewModel> Accounts { get; set; }
+
     public DateTime Created { get; set; }
+
+    public class AccountViewModel
+    {
+      public int AccountID { get; set; }
+      public string Name { get; set; }
+      public string Type { get; set; }
+    }
   }
 }
