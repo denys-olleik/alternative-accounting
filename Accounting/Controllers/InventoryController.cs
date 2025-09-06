f0dfb0c3bff7bf997104bcdb9a25fe5475b692d5
Denys Olleik <dolleik@gmail.com>
2025-09-06 06:32:15 -0400

    ref

--- Accounting/Controllers/InventoryController.cs
+++ Accounting/Controllers/InventoryController.cs
@@ -11,17 +11,38 @@ namespace Accounting.Controllers
   public class InventoryController : BaseController
   {
     private readonly AccountService _accountService;
+    private readonly InventoryService _inventoryService;
+    private readonly ItemService _itemService;
+    private readonly LocationService _locationService;
 
     public InventoryController(RequestContext requestContext)
     {
       _accountService = new AccountService(requestContext.DatabaseName!, requestContext.DatabasePassword!);
+      _inventoryService = new InventoryService(requestContext.DatabaseName!, requestContext.DatabasePassword!);
+      _itemService = new ItemService(requestContext.DatabaseName!, requestContext.DatabasePassword!);
+      _locationService = new LocationService(requestContext.DatabaseName!, requestContext.DatabasePassword!);
     }
 
-    [Route("create")]
+    [Route("create/{itemId}")]
     [HttpGet]
-    public async Task<IActionResult> Create()
+    public async Task<IActionResult> Create(int itemId)
     {
+      Item? item = await _itemService.GetAsync(itemId, GetOrganizationId()!.Value);
+      if (item == null)
+      {
+        return NotFound();
+      }
 
+      CreateInventoryViewModel createInventoryViewModel = new()
+      {
+        ItemId = itemId,
+        Locations = (await _locationService.GetAllAsync(GetOrganizationId()!.Value))
+          .Select(l => new CreateInventoryViewModel.LocationViewModel
+          {
+            LocationID = l.LocationID,
+            Name = l.Name
+          }).ToList()
+      };
 
       return View();
     }
--- Accounting/Models/InventoryViewModels/CreateInventoryViewModel.cs
+++ Accounting/Models/InventoryViewModels/CreateInventoryViewModel.cs
@@ -5,14 +5,23 @@
     public int ItemId { get; set; }
     public List<LocationViewModel> Locations { get; set; } = new();
     public int? SelectedLocationId { get; set; }
+    public List<Account> Accounts { get; set; } = new();
+    public int SelectedAccountId { get; set; }
     public int Quantity { get; set; }
     public int PriceAssetIndividual { get; set; }
     public int PriceAssetTotal { get; set; }
     public decimal? PriceInvoice { get; set; }
+    //public int CreditAccountId
 
     public class LocationViewModel
     {
       public int LocationID { get; set; }
+      public string Name { get; set; } = null!;
+    }
+
+    public class Account
+    {
+      public int AccountID { get; set; }
       public string Name { get; set; } = null!;
     }
   }
