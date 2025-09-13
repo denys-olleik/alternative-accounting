faade89d4d5f05b5fdd4f52dd4880865b712d982
Denys Olleik <dolleik@gmail.com>
2025-09-12 22:17:06 -0400

    ref

--- Accounting.Database/DapperPsqlFactory.cs
+++ Accounting.Database/DapperPsqlFactory.cs
@@ -8558,9 +8558,39 @@ namespace Accounting.Database
         throw new NotImplementedException();
       }
 
-      public Task<(List<Tax> taxes, int? nextPage)> GetAllAsync(int page, int pageSize, int organizationId)
+      public async Task<(List<Tax> taxes, int? nextPage)> GetAllAsync(
+        int page, 
+        int pageSize, 
+        int organizationId)
       {
-        throw new NotImplementedException();
+        DynamicParameters p = new DynamicParameters();
+        p.Add("@Page", page);
+        p.Add("@PageSize", pageSize);
+        p.Add("@OrganizationId", organizationId);
+        
+        IEnumerable<Tax> paginatedResult;
+
+        using (NpgsqlConnection con = new(_connectionString))
+        {
+          paginatedResult = await con.QueryAsync<Tax>($"""
+            SELECT * FROM (
+                SELECT *,
+                       ROW_NUMBER() OVER (ORDER BY "TaxID" DESC) AS RowNumber
+                FROM "Tax"
+            ) AS NumberedTaxes
+            WHERE RowNumber BETWEEN @PageSize * (@Page - 1) + 1 AND @PageSize * @Page + 1
+            AND "OrganizationId" = @OrganizationId
+            """, p);
+        }
+        
+        var result = paginatedResult.ToList();
+        int? nextPage = null;
+        if (result.Count > pageSize)
+        {
+          result.RemoveAt(result.Count - 1);
+          nextPage = page + 1;
+        }
+        return (result, nextPage);
       }
 
       public Tax Get(int id)
