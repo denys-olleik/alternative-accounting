3d84eb465f63783a475ffdf76f949a0113b78ede
Denys Olleik <dolleik@gmail.com>
2025-09-22 13:37:37 -0400

    ref

--- Accounting.Business/JournalInvoiceInvoiceLine.cs
+++ Accounting.Business/JournalInvoiceInvoiceLine.cs
@@ -2,16 +2,12 @@
 
 namespace Accounting.Business
 {
-  public class JournalInvoiceInvoiceLine : IIdentifiable<int>
+  public class JournalInvoiceInvoiceLine : JournalTableBase, IIdentifiable<int>
   {
     public int JournalInvoiceInvoiceLineID { get; set; }
-    public int JournalId { get; set; }
-    public Journal? Journal { get; set; }
     public int InvoiceId { get; set; }
     public int InvoiceLineId { get; set; }
     public int? ReversedJournalInvoiceInvoiceLineId { get; set; }
-    public Guid TransactionGuid { get; set; }
-    public DateTime Created { get; set; }
     public int CreatedById { get; set; }
     public int OrganizationId { get; set; }
 
