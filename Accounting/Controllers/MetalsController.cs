56341e4d664d8ad8686f5d898e4bc8e5be3700c7
Denys Olleik <dolleik@gmail.com>
2025-10-08 01:24:09 -0400

    ref

---
+++ Accounting/Controllers/MetalsController.cs
@@ -0,0 +1,16 @@
+﻿using Accounting.CustomAttributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Controllers
+{
+  [AuthorizeWithOrganizationId]
+  [Route("metals")]
+  public class MetalsController : BaseController
+  {
+    [Route("metals")]
+    public IActionResult Metals()
+    {
+      return View();
+    }
+  }
+}
