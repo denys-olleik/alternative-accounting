5fbd4ca786f1d7efbebda9eafeff32f13eeae9c5
Denys Olleik <dolleik@gmail.com>
2025-10-26 13:28:23 -0400

    ref

--- Accounting/Models/ReserveViewModels/MonetizeReserveViewModel.cs
+++ Accounting/Models/ReserveViewModels/MonetizeReserveViewModel.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation.Results;
+﻿using FluentValidation;
+using FluentValidation.Results;
 
 namespace Accounting.Models.ReserveViewModels
 {
@@ -8,5 +9,10 @@ namespace Accounting.Models.ReserveViewModels
     public decimal Amount { get; set; }
 
     public ValidationResult ValidationResult { get; set; } = new();
+
+    public class MonetizeReserveViewModelValidator : AbstractValidator<MonetizeReserveViewModel>
+    {
+
+    }
   }
 }
