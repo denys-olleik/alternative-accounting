ecf1a4f92fdd5e8a85aab4a7d7128358165293f3
Denys Olleik <dolleik@gmail.com>
2025-10-29 21:48:54 -0400

    ref

---
+++ Accounting/Controllers/DocumentationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Controllers
+{
+  public class DocumentationController : BaseController
+  {
+    public IActionResult Introduction()
+    {
+      return View();
+    }
+
+    public IActionResult Pricing()
+    {
+      return View();
+    }
+
+    public IActionResult Features()
+    {
+      return View();
+    }
+
+    public IActionResult Security()
+    {
+      return View();
+    }
+
+    public IActionResult GettingStarted()
+    {
+      return View();
+    }
+
+    public IActionResult Support()
+    {
+      return View();
+    }
+  }
+}
