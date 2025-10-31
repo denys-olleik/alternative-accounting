b89ac797d8b60141e622437c8febca451f1dd23f
Denys Olleik <dolleik@gmail.com>
2025-10-31 04:29:43 -0400

    ref

---
+++ Accounting/Controllers/VideoController.cs
@@ -0,0 +1,29 @@
+﻿using Accounting.CustomAttributes;
+using Accounting.Models.Item;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Controllers
+{
+  [AuthorizeWithOrganizationId]
+  [Route("video")]
+  public class VideoController : BaseController
+  {
+    [HttpGet]
+    [Route("videos")]
+    public IActionResult Videos(
+      int page = 1,
+      int pageSize = 2)
+    {
+      var referer = Request.Headers["Referer"].ToString() ?? string.Empty;
+
+      var vm = new VideosPaginatedViewModel
+      {
+        Page = page,
+        PageSize = pageSize,
+        RememberPageSize = string.IsNullOrEmpty(referer),
+      };
+
+      return View(vm);
+    }
+  }
+}
