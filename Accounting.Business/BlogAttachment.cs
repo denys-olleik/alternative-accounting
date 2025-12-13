e72079e8ffc77c4c3df97a17c2a5bf0ac082c89f
Denys Olleik <dolleik@gmail.com>
2025-12-13 04:07:38 -0500

    ref

--- Accounting.Business/BlogAttachment.cs
+++ Accounting.Business/BlogAttachment.cs
@@ -15,7 +15,7 @@ namespace Accounting.Business
 
     public int Identifiable => this.BlogAttachmentID;
 
-    public static class EncoderOptions
+    public static class EncoderOptionConstants
     {
       public const string Mp3 = "mp3";
       public const string Video720p = "720p";
@@ -23,9 +23,38 @@ namespace Accounting.Business
 
       private static readonly List<string> _all = new List<string>();
 
-      static EncoderOptions()
+      static EncoderOptionConstants()
       {
-        var fields = typeof(EncoderOptions)
+        var fields = typeof(EncoderOptionConstants)
+          .GetFields(
+            BindingFlags.Public
+            | BindingFlags.Static
+            | BindingFlags.DeclaredOnly);
+
+        foreach (var field in fields)
+        {
+          if (field.FieldType == typeof(string) && field.GetValue(null) is string value)
+          {
+            _all.Add(value);
+          }
+        }
+      }
+
+      public static IReadOnlyList<string> All => _all.AsReadOnly();
+    }
+
+    public static class BlogAttachmentEncoderStatusConstants
+    {
+      public const string None = "none";
+      public const string Queued = "queued";
+      public const string Processing = "processing";
+      public const string Completed = "completed";
+
+      private static readonly List<string> _all = new List<string>();
+
+      static BlogAttachmentEncoderStatusConstants()
+      {
+        var fields = typeof(BlogAttachmentEncoderStatusConstants)
           .GetFields(
             BindingFlags.Public
             | BindingFlags.Static
